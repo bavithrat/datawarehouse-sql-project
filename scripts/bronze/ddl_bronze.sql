c3af58d1ac28d58b7341fae1a13141347ea87c07
bavithrat <58383211+bavithrat@users.noreply.github.com>
2026-02-14 01:22:54 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,3 +1,13 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
 if object_id('bronze.crm_cust_info', 'U') is not null	      -- U refers to User table
 	drop table bronze.crm_cust_info;
